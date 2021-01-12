1ee33405916c913ce3426abf7a9694a88f2152fb
andikaleonardo <leonardodika.13@gmail.com>
2021-01-13 01:41:16 +0700

    init file

---
+++ src/Header.svelte
@@ -0,0 +1,9 @@
+<script>
+	export let title = 'Sport bracket';
+</script>
+
+<header>
+	<h1>
+		{title}
+	</h1>
+</header>
---
+++ src/stores.js
@@ -0,0 +1,29 @@
+import { writable } from 'svelte/store';
+import { shuffleTeams } from './utils.js';
+
+// starting from a hard-coded array of names
+const teams = [
+		'Turtles',
+		'Hummingbirds',
+		'Dolphins',
+		'Lambs',
+		'Lions',
+		'Giraffes',
+		'Wolves',
+		'Pellicans'
+	];
+
+// create a custom store with a .shuffle function, which allow to shuffle the array described in the store
+function createNames() {
+	const { subscribe, set, update } = writable(teams);
+	
+	return({
+		subscribe,
+		shuffle: () => update(n => n = shuffleTeams(n))
+	});
+}
+
+// export the custom store
+// the array is available through $names
+// it is updated through names.shuffle()
+export const names = createNames();
